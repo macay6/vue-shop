64260a6a00609bd1721acff69d89d512eb594b36
macay <1207260521@qq.com>
2020-12-09 23:51:47 +0800

    项目优化

---
+++ vue.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  // 发布模式
+  chainWebpack: config => {
+    config.when(process.env.NODE_ENV === 'production', config => {
+      config.entry('app').clear().add('./src/main-prod.js')
+      config.plugin('html').tap(args => {
+        args[0].isProd = true
+        return args
+      })
+    })
+    config.when(process.env.NODE_ENV === 'development', config => {
+      config.entry('app').clear().add('./src/main-dev.js')
+      config.plugin('html').tap(args => {
+        args[0].isProd = false
+        return args
+      })
+    })
+  }
+}
